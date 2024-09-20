d588f337b28e3c9949ec6b0fee9515697dc2fc6b
Zain2012-byte <muzaffartawseefzain@gmail.com>
2024-09-20 21:02:54 +0530

    learnt print statements

---
+++ main.py
@@ -0,0 +1,11 @@
+#printing a greeting message
+print("Hello,world!")
+
+#introducing my name
+print("My name is Zain")
+
+#printig my hobbies
+print("I love playing Taekwondo as well as Soccer")
+
+#adding two numbers
+print("Did you know adding 3+5 is:",3+5)
